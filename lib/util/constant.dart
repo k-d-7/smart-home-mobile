ff645841bdb049e963b1048a60f5e6d01d68b970
tranhungcuong-hub <80353022+tranhungcuong-hub@users.noreply.github.com>
2024-01-09 14:10:28 +0700

    first commit

---
+++ lib/util/constant.dart
@@ -0,0 +1,17 @@
+class APIConstant {
+  static const adafruitKey = "aio_ugOo69QCzTivkT2LYYZeRO8pCqlk";
+
+  static const fan =
+      "https://io.adafruit.com/api/v2/theloc3101/feeds/device1/data";
+  static const led =
+      "https://io.adafruit.com/api/v2/theloc3101/feeds/device2/data";
+  static const device3 =
+      "https://io.adafruit.com/api/v2/theloc3101/feeds/device3/data";
+  static const device4 =
+      "https://io.adafruit.com/api/v2/theloc3101/feeds/device4/data";
+
+  static const humidity =
+      "https://io.adafruit.com/api/v2/theloc3101/feeds/humidity/data";
+  static const temperature =
+      "https://io.adafruit.com/api/v2/theloc3101/feeds/temperature/data";
+}
